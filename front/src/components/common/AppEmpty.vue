d8e781a5b275407bcbe0307b922267619ae24be0
AGA80886 <s19910720@gmail.com>
2026-08-03 15:42:07 +0800

    feat: 建立共用 UI 元件 (#56)
    * feat: add shared UI components showcase
    
    * re

---
+++ front/src/components/common/AppEmpty.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-empty">
+    <ElEmpty
+      :description="description"
+      :image-size="imageSize"
+    >
+      <slot />
+    </ElEmpty>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  description?: string
+  imageSize?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  description: '目前沒有資料',
+  imageSize: 120,
+})
+</script>
+
+<style scoped lang="scss">
+.app-empty {
+  display: flex;
+  min-height: 240px;
+  padding: 2rem 1rem;
+  align-items: center;
+  justify-content: center;
+}
+</style>
